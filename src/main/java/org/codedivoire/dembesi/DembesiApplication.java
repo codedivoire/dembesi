538193941488795850ea004be1d4c2af3c4ebaee
Christian Amani <alvesamani@gmail.com>
2018-06-10 18:15:21 +0000

    Project configuration for future developments

---
+++ src/main/java/org/codedivoire/dembesi/DembesiApplication.java
@@ -0,0 +1,15 @@
+package org.codedivoire.dembesi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Christian Amani
+ */
+@SpringBootApplication
+public class DembesiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DembesiApplication.class, args);
+	}
+}
---
+++ src/test/java/org/codedivoire/dembesi/DembesiApplicationTests.java
@@ -0,0 +1,16 @@
+package org.codedivoire.dembesi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DembesiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
