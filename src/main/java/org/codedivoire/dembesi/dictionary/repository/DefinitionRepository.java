b8d9089ed3b1678908b8b2f2fb4372cf5fb4be9b
Christian Amani <alvesamani@gmail.com>
2018-08-23 09:16:12 +0000

    Add repositories of entities dictionary

---
+++ src/main/java/org/codedivoire/dembesi/dictionary/repository/DefinitionRepository.java
@@ -0,0 +1,12 @@
+package org.codedivoire.dembesi.dictionary.repository;
+
+import org.codedivoire.dembesi.dictionary.entity.Definition;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author Christian Amani on 23/08/2018.
+ */
+@Repository
+public interface DefinitionRepository extends JpaRepository<Definition,Long> {
+}
